51cb53b640fb364a9c709171af6269fd9de90d47
Daniil Nevynitsyn <97560239+CreperDN@users.noreply.github.com>
2025-06-01 05:45:07 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -72,7 +72,7 @@ async function fetchFilters(mainCategory, category, sizes, colors) {
   return {};
 }
 async function fetchPhotos(category = 'novinki', size = [], tsvet = [], page = 1) {
-  const url = "/api/api/product_by_categories_slugs";
+  const url = `${import.meta.env.VITE_API_BASE}/api/product_by_categories_slugs`;
 
   const headers = {
     "accept": "application/json, text/plain, */*",
